04a68dccbc120220ab88499e3b691962ea0546e4
emohamedd <a.elgaouej@gmail.com>
2023-10-18 17:04:30 +0100

    toupper with cout

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: emohamed <emohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 12:01:00 by emohamed          #+#    #+#             */
-/*   Updated: 2023/10/16 12:03:52 by emohamed         ###   ########.fr       */
+/*   Updated: 2023/10/18 17:03:50 by emohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@ int main(int ac , char **av)
         for (int i = 1; i < ac; i++)
         {
             for (int j = 0; av[i][j]; j++)
-                std::putchar(std::toupper(av[i][j]));
+            {
+                std::cout << (char)std::toupper(av[i][j]);
+            }
         }
     }
     std::cout << std::endl;
